d64453ab90a8f8a8abfb87a23387dbd9bd0a45ae
ofer morag <ofer.morag@hibob.io>
2018-06-19 23:22:34 +0300

    Fixed photo duplication upon sharing

--- ios/RNReactNativeSharingWinstagram.m
+++ ios/RNReactNativeSharingWinstagram.m
@@ -58,12 +58,14 @@ RCT_EXPORT_METHOD(share:(NSString *)base64Image copy:(NSString *)copy andUrl:(NS
 
 RCT_EXPORT_METHOD(shareWithInstagram:(NSString *)fileName
                   base64Image:(NSString *)base64Image
+                  localIdentifier:(NSString *)localIdentifier
                   successCallback:(RCTResponseSenderBlock)successCallback
                   failureCallback:(RCTResponseErrorBlock)failureCallback) {
     if ([[UIApplication sharedApplication] canOpenURL: [NSURL URLWithString:kInstagramURLScheme]]) {
         PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
         if (status == PHAuthorizationStatusAuthorized || status == PHAuthorizationStatusDenied) {
             [self savePicAndOpenInstagram: base64Image
+                          localIdentifier: localIdentifier
                           failureCallback: failureCallback
                           successCallback: successCallback];;
         }
@@ -71,8 +73,9 @@ RCT_EXPORT_METHOD(shareWithInstagram:(NSString *)fileName
             [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
                 if (status == PHAuthorizationStatusAuthorized) {
                     [self savePicAndOpenInstagram: base64Image
+                                  localIdentifier: localIdentifier
                                   failureCallback: failureCallback
-                                  successCallback: successCallback];;
+                                  successCallback: successCallback];
                 }
             }];
         }
@@ -162,34 +165,94 @@ RCT_EXPORT_METHOD(shareWithFacebook:(NSString *)copy andUrl:(NSString *)url
 
 # pragma mark - Helpers
 
+-(void)loadPhotoWithIdentifier:(NSString *)identifier resultHandler:(void (^)(UIImage *__nullable result, NSDictionary *__nullable info))resultHandler {
+    
+    PHAssetCollection *assetCollection = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeSmartAlbumUserLibrary options:nil].firstObject;
+
+    if (assetCollection == nil) {
+        if (resultHandler != nil) {
+            resultHandler(nil, nil);
+        }
+        
+        return;
+    }
+    
+    PHFetchOptions *fetchOptions = [PHFetchOptions new];
+    fetchOptions.predicate = [NSPredicate predicateWithFormat:@"localIdentifier = %@", identifier];
+    PHFetchResult<PHAsset *> *fetchResult = [PHAsset fetchAssetsInAssetCollection:assetCollection options:fetchOptions];
+    
+    if (fetchResult.count > 0) {
+        PHAsset *asset = fetchResult.firstObject;
+        PHImageRequestOptions *options = [PHImageRequestOptions new];
+        options.deliveryMode = PHImageRequestOptionsDeliveryModeHighQualityFormat;
+        options.networkAccessAllowed = YES;
+        options.synchronous = NO;
+        
+        [[PHImageManager defaultManager] requestImageForAsset:asset targetSize:CGSizeMake(asset.pixelWidth, asset.pixelHeight) contentMode:PHImageContentModeAspectFit options:options resultHandler:resultHandler];
+    } else {
+        if (resultHandler != nil) {
+            resultHandler(nil, nil);
+        }
+    }
+}
+
 -(void)savePicAndOpenInstagram:(NSString*)base64Image
+               localIdentifier:(NSString *)localIdentifier
+               failureCallback:(RCTResponseErrorBlock)failureCallback
+               successCallback:(RCTResponseSenderBlock)successCallback {
+    
+    NSURL *instagramURL = [NSURL URLWithString:[NSString stringWithFormat:kInstagramLibraryURLScheme, localIdentifier]];
+    
+    __weak typeof(RNReactNativeSharingWinstagram) *weakSelf = self;
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if ([[UIApplication sharedApplication] canOpenURL:instagramURL]) {
+            [weakSelf loadPhotoWithIdentifier:localIdentifier resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
+                if (result != nil) {
+                    [[UIApplication sharedApplication] openURL:instagramURL options:@{} completionHandler:NULL];
+                    if (successCallback != NULL) {
+                        successCallback(@[]);
+                    }
+                } else {
+                     [weakSelf helperForSavePicAndOpenInstagram:base64Image localIdentifier:localIdentifier failureCallback:failureCallback successCallback:successCallback];
+                }
+            }];
+            
+        } else {
+            [weakSelf helperForSavePicAndOpenInstagram:base64Image localIdentifier:localIdentifier failureCallback:failureCallback successCallback:successCallback];
+        }
+    });
+}
+
+-(void)helperForSavePicAndOpenInstagram:(NSString*)base64Image
+               localIdentifier:(NSString *)localIdentifier
                failureCallback:(RCTResponseErrorBlock)failureCallback
                successCallback:(RCTResponseSenderBlock)successCallback {
     UIImage *image = [UIImage imageWithData: [[NSData alloc]initWithBase64EncodedString:base64Image options:NSDataBase64DecodingIgnoreUnknownCharacters]];
-
+    
     if (!image) {
         return;
     }
-
+    
     NSURL *URL = [self nilOrFileURLWithImageDataTemporary:UIImageJPEGRepresentation(image, 0.9)];
-
+    
     __block PHAssetChangeRequest *_mChangeRequest = nil;
     __block PHObjectPlaceholder *placeholder;
-
+    
     [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
-
+        
         NSData *pngData = [NSData dataWithContentsOfURL:URL];
         UIImage *image = [UIImage imageWithData:pngData];
-
+        
         _mChangeRequest = [PHAssetChangeRequest creationRequestForAssetFromImage:image];
-
+        
         placeholder = _mChangeRequest.placeholderForCreatedAsset;
-
+        
     } completionHandler:^(BOOL success, NSError *error) {
-
+        
         if (success) {
             NSURL *instagramURL = [NSURL URLWithString:[NSString stringWithFormat:kInstagramLibraryURLScheme, [placeholder localIdentifier]]];
-
+            
             dispatch_async(dispatch_get_main_queue(), ^{
                 if ([[UIApplication sharedApplication] canOpenURL:instagramURL]) {
                     [[UIApplication sharedApplication] openURL:instagramURL options:@{} completionHandler:NULL];
@@ -203,10 +266,10 @@ RCT_EXPORT_METHOD(shareWithFacebook:(NSString *)copy andUrl:(NSString *)url
             if (failureCallback != NULL) {
                 failureCallback(error);
             }
-
+            
             NSLog(@"write error : %@",error);
         }
-    }];
+    }];\
 }
 
 - (NSURL *)nilOrFileURLWithImageDataTemporary:(NSData *)data {
